062572fa1bdf93356338504c7185d27d752fd2c4
Luca Lombardo <luke@pop-fisso.station>
2021-03-02 14:27:07 +0100

    incompleto

--- lezione-01/esercizio2.m
+++ lezione-01/esercizio2.m
@@ -1,9 +1,8 @@
-    % PUNTO 1
+ % PUNTO 1
 
-    %x = 0:0.01:0.5;
-    %plot(x, x.*sin(x), 'r')
+x = 0:0.01:0.5;
+plot(x, x.*sin(x), 'r')
 
-    % PUNTO 2
+% va continuata 
 
- t = linewidth(2)
- fplot ("t.*(3-t^2), 3.*t^2", [-2,2], 'g')
+
