6233ad37cc10c61cb04c294e6d14798e3841e6fd
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-03 17:09:44 +0100

    versione finale

--- lezione-00/fine.m
+++ lezione-00/fine.m
@@ -1,20 +1,6 @@
-%Plot sezioni del piano
-M=zeros(200);
-Z=linspace(-4,4,200);
-for i=1:200
-    for j=1:200
-        y=Z(i);
-        x=Z(j);
-        f=((x-1)^2+y^2-4)*((x+1)^2+y^2-4)*(x^2+(y-sqrt(3))^2-4);
-        if(f<1)
-            M(i,j)=1;
-        elseif(f<5)
-            M(i,j)=5;
-        elseif(f<10)
-            M(i,j)=10;
-        end
-    end
-end
-figure(3)
-imagesc(M)
-
+v = -5:0.01:5;
+[x y]=meshgrid(v);
+f =((x-1).^2+y.^2-4).*((x+1).^2+y.^2-4).*(x.^2+(y-sqrt(3)).^2-4) < 10;
+f = double(f);
+f(f == 0) = NaN;
+surf(x, y, f)
