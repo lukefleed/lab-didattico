a4e32382d9ba0572b6bc5d10744b0a70b5de9acd
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-08 16:34:43 +0100

    esercizio 1

--- lezione-01/es1.m
+++ lezione-01/es1.m
@@ -1,8 +1,10 @@
 function circ = es1(c,r)
+%prendo parte reale e immaginaria di c
 x = real(c);
 y = imag(c); 
-
+%definisco l'angolo dove deve stare
 th = 0:pi/50:2*pi;
-xunit = r * cos(th) + x;
-yunit = r * sin(th) + y;
+xunit = r*cos(th) + x;
+yunit = r*sin(th) + y;
+%si plotta
 circ = plot(xunit, yunit);
