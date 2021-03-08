7a9f8f6b7032cd8d2b052766beeb7b363257862f
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-08 17:41:43 +0100

    quasi finito

---
+++ lezione-01/es2.m
@@ -0,0 +1,19 @@
+%definire un vettore v di numeri complessi che rappresentino “tanti” punti equispaziati
+%sulla circonferenza di centro 1 e raggio 1
+
+th = 0:pi/100:2*pi;
+v = exp(i*th) + 1;
+%plotto tutti i puntini
+plot(v)
+axis([-10 10 -10 10],'equal');
+hold on
+
+%prima e ultima cifra del mio numero di matricola
+a = 6;
+b = 5;
+w = (a*v +b)./(v+1);
+plot(w)
+
+% DOMANDA: Qual `e la figura che ne risulta? Che cosa congetturate riguardo alle propriet`a della trasformazione di M¨obius che avete usato? Rispondete commentando lo script.
+% RISPOSTA: Congetturo che il raggio dopo la trasformazione è diminuito e la circonferenza  è stata traslata a destra
+
