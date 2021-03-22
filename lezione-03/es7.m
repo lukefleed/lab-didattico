a94885aec55bd0a686c0741ed7cf2a07e25ab888
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-22 18:55:32 +0100

    ora funziona

--- lezione-03/es7.m
+++ lezione-03/es7.m
@@ -1,23 +1,20 @@
 function es7(C,m)
-    n = size(C,1)/2;
-    for j = 1:n
-        dets(j) = abs(det(C(2*j-1:2*j,1:2)));
-    end
+	n=size(C,1)/2;
+	for j=1:n
+		dets(j)=abs(det(C(2*j-1:2*j,1:2)));
+	end
+	dets=max(dets,max(dets)/(25*n));
+	dets=dets/sum(dets);
+	p(1)=0;
+	for i=2:n
+		p(i)=p(i-1)+dets(i-1);
+	end
+	S(:,1)=[rand;rand];
+	for k=1:20+m
+		i=sum(p<rand);
+		S(:,k+1)=C(2*i-1:2*i,1:2)*S(:,k)+C(2*i-1:2*i,3);
+	end
+	plot(S(1,21:20+m),S(2,21:20+m),'m.')
+end
 
-    dets = max(dets,max(dets)/(25*n)
-    dets = dets/sum(dets);
-    p(1) = 0;
-    for j = 1:n
-        p(i) = p(i-1) + dets(i-1);
-    end
-
-    S(:,1)= [69 ; 420]; #numeri casuali
-        for k = 1:m 
-            r = rand;
-            i = sum(p<r);
-            S(:k+1) = C(2*i-1:2*i,1:2)*S(:,k) + C(2*i-1:2*1,3);
-        end
-    plot(S(1,21:20+m),S(2,21:20+m),'m.')
-
-endfunction 
 
