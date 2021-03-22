cbc6ee98287782720ab1b808b4b4bef2a6cd74db
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-22 18:55:45 +0100

    funziona

---
+++ lezione-03/koch.m
@@ -0,0 +1,12 @@
+# Definizioni
+S=[0.5 0 0;0 0.5 0;0.5 0 0.5;0 0.5 0;0.5 0 0.25; 0 0.5 0.5];
+x=1/3*sin(pi/3);
+s=1/6;
+t=1/3;
+t2=2/3;
+K=[t 0 0; 0 t 0;s -x t;x s 0;s x 0.5;-x s x;t 0 t2;0 t 0];
+
+#richiamo alla funzione precedente
+
+es7(S,100000);
+%es7(K,100000);
