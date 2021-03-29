a266da9d53678b8599b504d40135e07fe97bf497
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-29 17:33:58 +0200

    versione finale

--- lezione-04/es6.m
+++ lezione-04/es6.m
@@ -2,9 +2,9 @@ u = linspace(0,6*pi,50);
 v = linspace(0,2*pi,50);
 [U,V] = meshgrid(u,v);
 
-
 x = 2*(1 - exp(U/(6*pi))).*cos(U).*(cos(V/2)).^2;
 y = 2*(-1 + exp(U/(6*pi))).*sin(U).*(cos(V/2)).^2;
 z = 1 - exp(U/(3*pi)) - sin(V) + (exp(U/(6*pi))).*sin(V);
-axis equal
+
 mesh(x,y,z)
+
