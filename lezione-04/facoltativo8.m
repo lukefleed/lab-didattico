4d8bec835d410238c2deb9dc7c3f7860ae8cea05
Luca Lombardo <l.lombardo@protonmail.com>
2021-03-29 18:40:26 +0200

    funziona

---
+++ lezione-04/facoltativo8.m
@@ -0,0 +1,21 @@
+x = linspace(-200,200,400);
+y = linspace(-200,200,400);
+z = linspace(-200,200,400);
+
+[X,Y,Z] = meshgrid(x,y,z);
+
+a=(1+sqrt(5))/2; 
+
+p=(a*X-Y).*(a*X+Y);
+q=(a*Y-Z).*(a*Y+Z);
+r=(a*Z-X).*(a*Z+X);
+
+k=4*sqrt(5)-8;
+
+f=X.^2+Y.^2+Z.^2-1;
+rhs=k*p.*q.*r;
+F=f.^2-rhs;
+
+
+axis equal
+isosurface(X,Y,Z,F,0,X)
