5aeebc76f0d64b2f5615c67078106e5f78126bb6
Luca Lombardo <l.lombardo@protonmail.com>
2021-04-12 18:22:55 +0200

    funziona (spero)

--- lezione-05/es3.m
+++ lezione-05/es3.m
@@ -2,12 +2,15 @@ function es3(k)
     P1 = [1];
     P2 = [1 0];
     t = linspace(-1,1,1000);
+    hold on 
     plot(t,polyval(P1,t))
+    hold on
     plot(t,polyval(P2,t))
     for i = 3:k
         P3 = ((2*(i-1))*[P2 0] .- (i-1)*[0 0 P1])/i;
         P1 = P2;
         P2 = P3;
+        hold on
         plot(t, polyval(P3,t))
     end
    
