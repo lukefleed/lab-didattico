6af67826bc7db8ef440731106553fed3e7c5c505
Luca Lombardo <l.lombardo@protonmail.com>
2021-04-27 15:27:54 +0200

    sistemato

--- lezione-07/es1.m
+++ lezione-07/es1.m
@@ -1,4 +1,4 @@
-for n=100:100:10000
+for n=100:100:1000
     d = ones(1,n);
     sd= 0.5*ones(1,n-1); %sotto diagonale
     Sd= 0.1*ones(1,n-1); %sopra diagonale
@@ -9,18 +9,20 @@ for n=100:100:10000
     %FULL
     tic
     [L,U]=lu(A);
-    time_lu_full=toc;
+    time_lu_full(n/100)=toc;
 
 
     %SPARSE
     B = sparse(A);
     tic
     [L,U]=lu(B);
-    time_lu_sparse=toc;
+    time_lu_sparse(n/100)=toc;
 
-    %Si plotta
+   
+end 
+
+%Si plotta
     hold on
-    plot(time_lu_full)
-    plot(time_lu_sparse)
-    wait 2
-end
+    plot(time_lu_full,'r')
+    plot(time_lu_sparse,'b')
+    pause(1)
