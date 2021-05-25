eb2a81a3b9b20382436d15f5ff33de576eb78d4a
Luca Lombardo <l.lombardo@protonmail.com>
2021-05-25 11:10:09 +0200

    funziona

--- lezione-10/neldermead.m
+++ lezione-10/neldermead.m
@@ -1,11 +1,20 @@
 function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
-	k=0;
+
 	f1=f(x1(1),x1(2));
 	f2=f(x2(1),x2(2));
 	f3=f(x3(1),x3(2));
+
+	k=0;
 	while((norm(x2-x1,2)+norm(x3-x1,2)>epsilon)&&(abs(f2-f1)+abs(f3-f1)>epsilon)&&(k<itermax))
 		k+=1;
 		X=[x1;x2;x3];
+
+		# Triangoli
+		line([x1(1) x2(1)],[x1(2) x2(2)],'Color','r');
+		hold on;
+		line([x2(1) x3(1)],[x2(2) x3(2)],'Color','r');
+		line([x3(1) x1(1)],[x3(2) x1(2)],'Color','r');
+
 		[F,I]=sort([f1,f2,f3]);
 		x1=X(I(1),:);
 		x2=X(I(2),:);
@@ -13,10 +22,12 @@ function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
 		f1=F(1);
 		f2=F(2);
 		f3=F(3);
+
 		xm=(x1+x2)/2;
 		xr=2*xm-x3;
 		fr=f(xr(1),xr(2));
 		fm=f(xm(1),xm(2));
+
 		if((f1<=fr)&&(fr<f2))
 			x3=xr;
 		elseif(fr<f1)
@@ -27,7 +38,7 @@ function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
 			else
 				x3=xr;
 			end
-		elseif(fr>=f2)
+		else
 			xc=(xm+xr/2);
 			fc=f(xc(1),xc(2));
 			if(fc<f3)
@@ -37,15 +48,15 @@ function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
 				x3=(x3+x1)/2;
 			end
 		end
-	f1=f(x1(1),x1(2));
-	f2=f(x2(1),x2(2));
-	f3=f(x3(1),x3(2));
+		f1=f(x1(1),x1(2));
+		f2=f(x2(1),x2(2));
+		f3=f(x3(1),x3(2));
 	end
 
 	xmin=x1;
 
 	if(k>=itermax)
-		printf("Raggiunto il numero massimo di iterazioni.\n");
+		printf("Fallimento: raggiunto il massimo numero di iterazioni.\n");
 	end
 
 end
