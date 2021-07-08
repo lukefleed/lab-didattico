0ab9fc39d68c4d4f31a7cb3493cb65a3645371be
Luca Lombardo <l.lombardo@protonmail.com>
2021-07-08 18:54:57 +0200

    piccola correzione

--- lezione-05/es3.m
+++ lezione-05/es3.m
@@ -1,17 +1,14 @@
-function es3(k)
-    P1 = [1];
-    P2 = [1 0];
-    t = linspace(-1,1,1000);
-    hold on 
-    plot(t,polyval(P1,t))
-    hold on
-    plot(t,polyval(P2,t))
-    for i = 3:k
-        P3 = ((2*(i-1))*[P2 0] .- (i-1)*[0 0 P1])/i;
-        P1 = P2;
-        P2 = P3;
-        hold on
-        plot(t, polyval(P3,t))
-    end
-   
-endfunction 
+function es3(K)
+	P1=[1];
+	P2=[1 0];
+	t=linspace(-1,1,100);
+	hold on
+	plot(t,polyval(P1,t))
+	plot(t,polyval(P2,t))
+	for i=3:K
+		P3=((2*i-1)*[P2 0]-(i-1)*[0 0 P1])/i;
+		P1=P2;
+		P2=P3;
+		plot(t,polyval(P3,t))
+	end
+end
