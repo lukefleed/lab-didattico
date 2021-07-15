4e6216e150a66eaf40bfebac84df85cde58979db
Luca Lombardo <l.lombardo@protonmail.com>
2021-07-15 23:24:18 +0200

    ora dovrebbe andare

--- lezione-08/es3.m
+++ lezione-08/es3.m
@@ -4,9 +4,10 @@ function S = es3(f,a,b,N)
     somma2 = 0;
     fine = N/2 -1;
         for i = 1:fine
-            somma1 += f(a + 2*i*h);
-            somma2 += f(a + (2*i -1)*h); 
+            somma1+=f(a + 2*i*h);
+            somma2+=f(a + (2*i-1)*h); 
         end
-    S = (h/3)*(f(a)+f(b) + 2*somma1 + 4*somma2);
+        somma2+=f(a+(N-1)*h);
+        S = (h/3)*(f(a)+f(b) + 2*somma1 + 4*somma2);
 end
 
