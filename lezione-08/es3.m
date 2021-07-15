0b160a7d7334da9d13a83c61233d4b9323839e9c
Luca Lombardo <l.lombardo@protonmail.com>
2021-07-15 23:19:08 +0200

    end

--- lezione-08/es3.m
+++ lezione-08/es3.m
@@ -1,11 +1,12 @@
 function S = es3(f,a,b,N)
-h = (b-a)/N;
-somma1 = 0;
-somma2 = 0;
-fine = N/2 -1;
-    for i = 1:fine
-        somma1 += f(a + 2*i*h);
-        somma2 += f(a + (2*i -1)*h); 
-    end
-S = (h/3)*(f(a)+f(b) + 2*somma1 + 4*somma2);
+    h = (b-a)/N;
+    somma1 = 0;
+    somma2 = 0;
+    fine = N/2 -1;
+        for i = 1:fine
+            somma1 += f(a + 2*i*h);
+            somma2 += f(a + (2*i -1)*h); 
+        end
+    S = (h/3)*(f(a)+f(b) + 2*somma1 + 4*somma2);
+end
 
