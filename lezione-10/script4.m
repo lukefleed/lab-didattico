f86f7502eab41df2ea96d69ded64e2719f42acca
Luca Lombardo <l.lombardo@protonmail.com>
2021-07-15 22:59:36 +0200

    punti iniziali

--- lezione-10/script4.m
+++ lezione-10/script4.m
@@ -12,8 +12,6 @@ shading flat
 
 hold on
 
-min=neldermead(f,10^(-2),100,[-1,1],[-1,0],[2,1/2])
+min=neldermead(f,10^(-2),100,[1,1],[0,0],[2,0])
 
-pause(10)
-
-## Per qualche motivo Octave non riesce a plottare i triangoli, utilizzando GnuPlot si ottiene invece il risultato desiderato. Non testato su matlab 
+pause(60)
